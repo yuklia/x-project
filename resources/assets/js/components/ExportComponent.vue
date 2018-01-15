5613e72ae45d3759df8800cf0534c410761453ce
Yulia Kostrikova <y.kostrikova@nixsolutions.com>
2018-01-16 01:06:31 +0200

    Add ExportComponent.vue

---
+++ resources/assets/js/components/ExportComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">
+        <div><img src="/images/logo_sm.jpg" alt="Logo" title="logo"></div>
+        <div style='margin: 10px;  text-align: left'>
+            <input type="button" value="Select All"/>
+            <form action="/export-all">
+                <input type="submit" value="Export All"/>
+            </form>
+
+            <form action="/export-course-attendance">
+                <input type="submit" value="Export Course Attendance"/>
+            </form>
+
+        </div>
+    </div>
+</template>
