8837252abe564139627cba06d608332897f2f5a6
Yulia Kostrikova <y.kostrikova@nixsolutions.com>
2018-01-16 10:32:27 +0200

    Lost students data right after component mounted

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,25 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import axios from 'axios';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('export-component', require('./components/ExportComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        students: [],
+    },
+    mounted : function() {
+        this.getStudents();
+    },
+    methods: {
+        getStudents() {
+            let _this = this;
+            axios.get('api/v1/students').then(response => {
+                _this.students = response.data;
+            });
+        }
+    }
 });
